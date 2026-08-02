4637f407e5f4568a367b04933445aa6ea6d49f1f
yuliyatestlab <yuliya.testlab@gmail.com>
2026-08-02 15:13:52 +0100

    Update for deploy

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, current_app, Blueprint, url_for, redirect, request, flash, abort, session
-from markupsafe import Markup
+from flask import Flask, render_template, current_app, Blueprint, url_for, redirect, Markup, request, flash, abort, session
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from flask_sqlalchemy import SQLAlchemy
