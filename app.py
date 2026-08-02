9240f123c0cbeecc3c2bc23d81f68a73793bec44
yuliyatestlab <yuliya.testlab@gmail.com>
2026-08-02 12:11:52 +0100

    Update requirements.txt fix typo.

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from functools import wraps
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY', '8BYkEfBA6O6donzWlSihBXox7C0sKR6b')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
