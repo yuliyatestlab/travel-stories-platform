b9b22ab3235bfa59099f9687f3eeea7316c00573
yuliyatestlab <yuliya.testlab@gmail.com>
2026-08-03 10:34:09 +0100

    Update port for deploy.

--- app.py
+++ app.py
@@ -247,4 +247,8 @@ def admin_panel():
     pass
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
+        debug=False
+    )
